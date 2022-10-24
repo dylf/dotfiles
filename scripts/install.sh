94f53435a3f381074da5e3f50ddc969b3bd41f3b
Dylan Fontaine <dylanfontaine@live.ca>
2022-10-24 13:31:05 -0700

    remove things that have moved to ansible playbook

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,31 +5,11 @@ sudo apt install -y \
 	alacritty \
 	apt-transport-https \
 	curl \
-	fzf \
-	gnome-tweaks \
-	vim \
-	zsh 
-
-# Install brave
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update -y
-sudo apt install brave-browser -y
-
-# Install neovim
-curl -fsSLo ~/.local/bin/nvim https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
-chmod +x ~/.local/bin/nvim
-sudo ln -s ~/.local/bin/nvim /usr/bin/nvim
+	gnome-tweaks
 
 # Install fonts
-mkdir -p ~/.local/share/fonts
-curl https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/VictorMono.zip -fsSLo /tmp/VictorMonoAll.zip
-unzip /tmp/VictorMonoAll.zip -d ~/.local/share/fonts/VictorMono
-rm /tmp/VictorMonoAll.zip
 fc-cache -fv
 
 # Set keybindings
 dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:escape']"
 
-# Install fnm
-curl -fsSL https://fnm.vercel.app/install | bash -s -- --install-dir "$HOME/.local/bin" --skip-shell
