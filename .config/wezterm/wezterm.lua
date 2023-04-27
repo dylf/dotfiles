4af1a6f14e0cf6062382ee7aa777e646187f77b2
Dylan Fontaine <me@dylf.dev>
2023-04-26 17:14:10 -0700

    update wezterm to rosepine

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,12 +4,13 @@ local config = {}
 config.font = wezterm.font("VictorMono Nerd Font", { weight = "Medium" })
 config.font_size = 15.0
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
-config.color_scheme = "carbonfox"
+config.color_scheme = "rose-pine"
 
 config.window_decorations = "RESIZE"
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.force_reverse_video_cursor = true
+-- config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 
 config.background = {
 	{
@@ -17,7 +18,7 @@ config.background = {
 			File = "Pictures/termbg.png",
 		},
 		hsb = {
-			brightness = 0.01,
+			brightness = 0.1,
 		},
 	},
 }
