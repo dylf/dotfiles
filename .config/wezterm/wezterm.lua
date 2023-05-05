0c4ca64785646ea826a20331f80b0696c60744f9
Dylan Fontaine <me@dylf.dev>
2023-05-04 18:23:17 -0700

    unironically switch terminal font to comic sans

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,8 +1,15 @@
 local wezterm = require("wezterm")
 local config = {}
 
-config.font = wezterm.font("VictorMono Nerd Font", { weight = "Medium" })
-config.font_size = 15.0
+config.font = wezterm.font_with_fallback({
+  { family = "ComicShannsMono Nerd Font" },
+  {
+    family = "VictorMono Nerd Font",
+    weight = "Medium"
+  },
+})
+
+config.font_size = 16.0
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.color_scheme = "rose-pine"
 
@@ -13,14 +20,14 @@ config.force_reverse_video_cursor = true
 -- config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 
 config.background = {
-	{
-		source = {
-			File = "Pictures/termbg.png",
-		},
-		hsb = {
-			brightness = 0.1,
-		},
-	},
+  {
+    source = {
+      File = "Pictures/termbg.png",
+    },
+    hsb = {
+      brightness = 0.05,
+    },
+  },
 }
 
 config.adjust_window_size_when_changing_font_size = false
