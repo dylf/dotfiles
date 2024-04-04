6bd201fe6a297f3fbc25f755e87663442d4be12f
Dylan Fontaine <10430358+dylf@users.noreply.github.com>
2024-04-03 17:31:18 -0700

    add lando to path

--- .config/zsh/includes/path.zsh
+++ .config/zsh/includes/path.zsh
@@ -6,5 +6,5 @@ export PATH=~/.local/share/solana/install/active_release/bin:$PATH
 export PATH=~/go/bin:$PATH
 export PATH=~/.local/share/JetBrains/Toolbox/scripts:$PATH
 export PATH=~/.config/composer/vendor/bin:$PATH
+export PATH="/usr/share/lando/bin:~/.lando/bin${PATH+:$PATH}"; #landopath
 # END PATH UPDATES
-
