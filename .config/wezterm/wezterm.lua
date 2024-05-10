e63728cef87288cf8bf1def17c9bc15f930309a2
Dylan Fontaine <10430358+dylf@users.noreply.github.com>
2024-05-09 17:26:09 -0700

    chore: wezterm config updates

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,22 +3,24 @@ local config = {}
 
 config.font = wezterm.font_with_fallback({
 	{ family = "SpaceMono Nerd Font" },
-	{ family = "ComicShannsMono Nerd Font" },
+	{ family = "Comic Mono" },
+	{ family = "Iosevka" },
 	{
 		family = "VictorMono Nerd Font",
 		weight = "Medium",
 	},
 })
 
-config.font_size = 16.0
-config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+config.font_size = 18.0
+config.harfbuzz_features = { "calt=0", "clig=1", "liga=0" }
+
 config.color_scheme = "rose-pine"
 
 config.window_decorations = "RESIZE"
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.force_reverse_video_cursor = true
-config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+config.window_padding = { left = 4, right = 4, top = 0, bottom = 0 }
 
 config.background = {
 	{
@@ -34,6 +36,7 @@ config.background = {
 
 --- Disable bg
 config.background = {}
+config.window_background_opacity = 0.95
 
 config.adjust_window_size_when_changing_font_size = false
 
